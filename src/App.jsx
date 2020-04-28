d5d2fb75b1d241403f67c45be3269a68e43aeaef
Caleb Barger <bargercj20@hotmail.com>
2020-04-28 12:49:39 -0700

    everything is workable now :D

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, {useState, useReducer} from 'react';
 import * as yup from 'yup'
 import {Switch, Route} from 'react-router-dom'
-import axios from 'axios'
+// import axios from 'axios'
 
 // Styles 
 import './App.css';
@@ -24,7 +24,7 @@ const formSchema = yup.object().shape({
     .required('a valid password is required')
 })
 
-const url = ''
+// const url = ''
 
 
 const initalFormValues = {
@@ -37,9 +37,10 @@ const initalFormErrors = {
 }
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [formValues, setFormValues] = useState(initalFormValues)
   const [formErrors, setFormErrors] = useState(initalFormErrors)
-  const [user, setUser] = useState({})
+  // const [user, setUser] = useState({})
 
   const onChangeHandler = evt => {
     const name = evt.target.name
@@ -67,34 +68,23 @@ function App() {
     })
   }
 
-  const postUser = user => {
-    axios.post(url, user)
-    .then(res => {
-        setUser(res.data)
-    })
-    .catch(err => {
-      debugger
-    })
-  }
-
-  // const signUpHandler = evt => {
-  //   evt.preventDefault()
-
-  //   const newUser =  
-  //   console.log(formValues)
+  // const postUser = user => {
+  //   axios.post(url, user)
+  //   .then(res => {
+  //       setUser(res.data)
+  //   })
+  //   .catch(err => {
+  //     debugger
+  //   })
   // }
+
+  const onSubmitHandler = evt => {
+    evt.preventDefault()
+    console.log(formValues)
+  }
 
   return (
     <Switch>
-      <Route exact path='/'>
-        <Login 
-          formValues={formValues}
-          onChangeHandler={onChangeHandler}
-          // onSubmitHandler={onSubmitHandler}
-          signU
-          formErrors={formErrors}
-        />
-      </Route>
       <Route path='/valuelist'>
        
         <ValueList values = {state.values} dispatch = {dispatch} />
@@ -103,6 +93,14 @@ function App() {
       <Route path='/selectedvalues'>
        
         <SelectedValues values = {state.values} dispatch = {dispatch} />
+      </Route>
+      <Route path='/'>
+        <Login 
+          formValues={formValues}
+          onChangeHandler={onChangeHandler}
+          onSubmitHandler={onSubmitHandler}
+          formErrors={formErrors}
+        />
       </Route>
     </Switch>
   )
