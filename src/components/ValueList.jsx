603181541439699432b880d3f846539dd93da932
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-04-29 17:46:03 -0700

    got my redux fetch data to work

--- src/components/ValueList.jsx
+++ src/components/ValueList.jsx
@@ -5,68 +5,61 @@ import Button from '@material-ui/core/Button';
 import { useHistory } from 'react-router-dom';
 import { fetchData, selectItem } from '../actions/actions';
 import { connect } from 'react-redux';
-import {axiosWithAuth} from "../utils/axiosAuth";
+import { axiosWithAuth } from "../utils/axiosAuth";
 import { ValuesContext } from "../contexts";
 
 
-    const ValueList = props => {
+const ValueList = props => {
 
-        const { push } = useHistory();
-        const [data, setData] = useState([]);
+    const { push } = useHistory();
+   
 
-        const {fetchingData, initialState} = useContext(ValuesContext);
-        useEffect(() => {
-            // fetchingData();
-            
+   
+    useEffect(() => {
+   
 
-          props.fetchData();
-            // for some reason can't get this action creator to work :( 
-               
-            axiosWithAuth()
-             .get("https://essentialism-bwt.herokuapp.com/api/values")
-         .then(res => {
-                setData(res.data);
-             })
 
-           // setData(initialState[0]);
-
-         }, [])
-
-        
-       // console.log(props.values);
+        props.fetchData();
     
-        const nextPage =  e => {
-            e.preventDefault();
-            push('/dashboard');
-           
-        }
 
-       
+    }, [])
 
-console.log("data", data);
+
+   
+
+    const nextPage = e => {
+        e.preventDefault();
+        push('/dashboard');
+
+    }
+
+
+
+    console.log("data", props.values);
 
 
     return (
 
         <div id="valueCont">
-            
+
             <h1>Which values resonate with you?</h1>
             <ValueForm values={props.values} />
-            <div  id="valueGrid">
+            <div id="valueGrid">
 
-           {data ? data.map(value => {
-                return (
-                    <ValueCard value = {value} key = {value.id} selectItemList = {props.selectItemList}  />
-                )
-    }) : null}
-           
-            
-        </div>
-        <Button id = "nextButton" color="primary" variant="contained" onClick = {nextPage} >
-      Next
+                {props.values?.map(value => {
+                    console.log(value)
+                    return (
+                        <ValueCard value={value} key={value.id} selectItemList={props.selectItemList} />
+                    )
+                }) }
+
+
+            </div>
+            <Button id="nextButton" color="primary" variant="contained" onClick={nextPage} >
+                Next
     </Button>
-            
-           
+
+
         </div>
     )
 };
